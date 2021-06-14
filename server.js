17a35a9c236162819ede424de9f2729d81db4647
enPlace <n.place@protonmail.com>
2021-06-14 11:38:25 -0300

    mongoose setup

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("working")
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/subscribers', { useNewUrlParser: true, useUnifiedTopology: true  })
+
+app.listen(5000, (req, res)=>{
+    console.log("server listening on port 5000...")
+})
+
